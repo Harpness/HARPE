7b1418d914e2eb69d81b864e58b27cb8abe8c731
Harpness <143333833+Harpness@users.noreply.github.com>
2024-06-15 20:22:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,34 @@
-echo Mise à jour du serveur...
+#!/bin/bash
+
+echo "Mise à jour du serveur..."
 sudo apt-get update
-echo Installation de pip...
-sudo apt install pip -y
+
+echo "Installation de pip..."
+sudo apt install python3-pip -y
+
+# Création d'un fichier de log pour cron
 touch cronlog.log
 
-echo Installation des packages nécessaires...
+echo "Installation des packages nécessaires..."
 cd Live-Tools-V2
-sudo apt-get install python3-venv
+
+# Installation de l'environnement virtuel
+sudo apt-get install python3-venv -y
 python3 -m venv .venv
+
+# Activation de l'environnement virtuel
 source .venv/bin/activate
+
+# Installation des dépendances depuis requirements.txt
 pip install -r requirements.txt
+
+# Ajout des packages nécessaires pour le script de trading
+pip install ta pandas scikit-learn joblib ccxt pydantic
+
+# Configurer git pour ignorer les modifications de secret.py
 git update-index --assume-unchanged secret.py
+
+# Retour au répertoire précédent
 cd ..
+
+echo "Configuration terminée."
